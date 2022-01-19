6fe136c1b1a185ec50f88f33371e73c76682c71d
SON CHAU <soncqb@gmail.com>
2022-01-18 23:46:15 -0800

    add fixture

--- tests/test_features_creator.py
+++ tests/test_features_creator.py
@@ -1,1 +1,20 @@
-from features_creator import features_creator
+
+
+import pandas as pd
+import numpy as np
+import pytest
+
+from features_creator.features_creator import *
+
+
+@pytest.fixture
+def data_df():
+    data = {
+        "week_payment1": [1.0, 2, 3],
+        "week_payment2": [4, 5.0, 6],
+        "week_payment3": [7, 8, 9.0],
+        "othercolumn": [1, 1, 1]}
+    df = pd.DataFrame(data)
+    return df
+
+
