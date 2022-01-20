f36903c42ba41a3f9a5d42c5876ff0ec0996ecb0
PANDASANG1231 <zwj63518583@gmail.com>
2022-01-19 19:13:11 -0800

    Implement test_calculate_standard_deviation

--- tests/test_features_creator.py
+++ tests/test_features_creator.py
@@ -1,1 +1,76 @@
-from features_creator import features_creator
+import pandas as pd
+import numpy as np
+import pytest
+
+from features_creator.features_creator import *
+
+
+test_df = pd.DataFrame({
+        "week_payment1": [1.0, 2, 3],
+        "week_payment2": [4, 5.0, 6],
+        "week_payment3": [7, 8, 9.0],
+        "othercolumn": [1, 1, 1]
+})
+
+@pytest.mark.parametrize(
+    'json',
+    (
+        ### Check data type of the first argument:data
+        {"data": 3.141, "pattern": "week_payment", "check": "TypeErrorInput1"},
+        {"data": "test.txt", "pattern": "week_payment", "check": "TypeErrorInput1"},
+        {"data": ["list", "of", "words"], "pattern": "week_payment", "check": "TypeErrorInput1"},
+        ### Check data type of the second argument:pattern
+        {"data": test_df, "pattern": 3.14, "check": "TypeErrorInput2"},
+        {"data": test_df, "pattern": pd.DataFrame([]), "check": "TypeErrorInput2"},
+        {"data": test_df, "pattern": ["list", "of", "words"], "check": "TypeErrorInput2"},
+        ### Check if the dataframe has non-numeric values
+        {"data": test_df.astype(str), "pattern": "week_payment", "check": "TypeErrorNumeric"},
+        
+        ### Check data type of the output
+        {"data": test_df, "pattern": "week_payment", "check": "TypeErrorOutput"},
+        ### Check accuracy of the output
+        {"data": test_df, "pattern": "week_payment", "check": "AccuracyOutput"}
+    )
+)
+def test_calculate_standard_deviation(json):
+    
+    """Check TypeError raised when input is not data frame."""
+    
+    if json["check"] == "TypeErrorInput1":
+        with pytest.raises(TypeError):
+            print(json["data"], json["pattern"])
+            calculate_standard_deviation(json["data"], json["pattern"])
+            
+
+    if json["check"] == "TypeErrorInput2":
+        with pytest.raises(TypeError):
+            print(type(json["data"]), json["pattern"])
+            calculate_standard_deviation(json["data"], json["pattern"])
+            
+            
+    if json["check"] == "TypeErrorNumeric":
+        with pytest.raises(TypeError):
+            print(type(json["data"]), json["pattern"])
+            calculate_standard_deviation(json["data"], json["pattern"])
+            
+            
+    if json["check"] == "TypeErrorOutput":
+        assert isinstance(calculate_standard_deviation(json["data"], json["pattern"]), pd.DataFrame), \
+            "Returned the wrong data type, output should be data frame"
+            
+            
+    if json["check"] == "AccuracyOutput":
+        # Test if the function return correct value when there is only one element
+        assert calculate_standard_deviation(test_df.iloc[0:1, 0:1], "week_payment").values == np.array([[0]]), \
+            "Should return [0], if input data frame has only one element"
+        # Test if the function return correct value when there is only one column
+        assert np.array_equal(calculate_standard_deviation(test_df.iloc[:, 0:1], "week_payment").values, np.array([[0]]*test_df.shape[0])), \
+            "Should a column of 0, if input data frame has only one column"
+        # Test if the function return correct value 
+        assert np.array_equal(calculate_standard_deviation(test_df, "week_payment").values, np.array([[6.], [6.], [6.]])), \
+            "The result is not right"
+
+
+        
+    
+    
